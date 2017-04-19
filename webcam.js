41cb34e3747e2bd8a88d894ec154c11a17b1b052
Lewis Freiberg <lewi@2pk.in>
2017-04-19 23:40:36 +1000

    stop rather than read

--- webcam.js
+++ webcam.js
@@ -5,7 +5,7 @@ var camera = new RaspiCam({ mode: 'photo', output: './images/photo.jpg'});
 var capture = function (time) {
   var p = new Promise( function( res, rej ) {
       camera.start();
-      camera.on("read", function(err, timestamp, filename) {
+      camera.on("stop", function(err, timestamp, filename) {
       	console.log('Image Captured at ' + filename);
         res("./images/photo.jpg");
       });
