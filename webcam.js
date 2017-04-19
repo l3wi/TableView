ebc238d6dd9b8e61c729ae3314899328a6ffe60f
Lewis Freiberg <lewi@2pk.in>
2017-04-19 23:33:40 +1000

    Change File location

--- webcam.js
+++ webcam.js
@@ -5,10 +5,9 @@ var camera = new RaspiCam({ mode: 'photo', output: './images/photo.jpg'});
 var capture = function (time) {
   var p = new Promise( function( res, rej ) {
       camera.start();
-      camera.on("read", function(err, timestamp, filename){
-      	//do stuff
+      camera.on("read", function(err, timestamp, filename) {
       	console.log('Image Captured at ' + filename);
-        res("./images/" + time + ".jpg");
+        res("./images/photo.jpg");
       });
     });
   return p;
