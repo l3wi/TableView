02b63271b7de84096185eb39e49bd63182a21042
Lewis Freiberg <lewi@2pk.in>
2017-04-19 23:43:32 +1000

    Stopping Camera Now

--- webcam.js
+++ webcam.js
@@ -5,6 +5,13 @@ var camera = new RaspiCam({ mode: 'photo', output: './images/photo.jpg'});
 var capture = function (time) {
   var p = new Promise( function( res, rej ) {
       camera.start();
+
+      camera.on("read", function(err, timestamp, filename){
+        console.log('Stopping Camera');
+         setTimeout(function () {
+           camera.stop();
+         }, 500);
+      });
       camera.on("stop", function(err, timestamp, filename) {
       	console.log('Image Captured at ' + filename);
         res("./images/photo.jpg");
