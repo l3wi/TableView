1ee6deda69ab1a3bf814e891c899466dcccdc579
Lewis Freiberg <lewis.freiberg@cheproximity.com.au>
2017-07-07 14:23:54 +1000

    Udate to upload to channel

--- toby.js
+++ toby.js
@@ -1,101 +1,93 @@
-'use strict';
+"use strict";
 
-var util = require('util');
-var path = require('path');
-var fs = require('fs');
-var Bot = require('slackbots');
-var Webcam = require('./webcam');
-var moment = require('moment');
-var imgur = require('imgur');
+var util = require("util");
+var path = require("path");
+var fs = require("fs");
+var Bot = require("slackbots");
+var Webcam = require("./webcam");
+var moment = require("moment");
+var imgur = require("imgur");
+var Slack = require("node-slack-upload");
 
 var Toby = function Constructor(settings) {
-    this.settings = settings;
-    this.settings.name = this.settings.name || 'Toby';
-    this.user = null;
+  this.settings = settings;
+  this.settings.name = this.settings.name || "Toby";
+  this.user = null;
 };
 
 // inherits methods and properties from the Bot constructor
 util.inherits(Toby, Bot);
 
+Toby.prototype.run = function() {
+  Toby.super_.call(this, this.settings);
 
-Toby.prototype.run = function () {
-    Toby.super_.call(this, this.settings);
-
-    this.on('start', this._onStart);
-    this.on('message', this._onMessage);
+  this.on("start", this._onStart);
+  this.on("message", this._onMessage);
 };
 
-
-Toby.prototype._onStart = function () {
-    this._loadBotUser();
+Toby.prototype._onStart = function() {
+  this._loadBotUser();
 };
 
-
-Toby.prototype._onMessage = function (message) {
-    if (this._isChatMessage(message) &&
-        this._isChannelConversation(message) &&
-        !this._isFromToby(message) &&
-        this._isMentioningToby(message)
-    ) {
-        this._sayHi(message);
-    }
+Toby.prototype._onMessage = function(message) {
+  if (
+    this._isChatMessage(message) &&
+    this._isChannelConversation(message) &&
+    !this._isFromToby(message) &&
+    this._isMentioningToby(message)
+  ) {
+    console.log("Got Message");
+    this._sayHi(message);
+  }
 };
 
-
-
-Toby.prototype._sayHi = function (originalMessage) {
-    var self = this;
-    var now = moment().format('x');
-    Webcam(now).then( function(location) {
+Toby.prototype._sayHi = function(originalMessage) {
+  var self = this;
+  var now = moment().format("x");
+  Webcam(now)
+    .then(function(location) {
       self._uploadImage(originalMessage, location);
     })
-    .catch(function (er) {
+    .catch(function(er) {
       console.error(err.message);
     });
 };
 
-Toby.prototype._uploadImage = function (originalMessage, location) {
+Toby.prototype._uploadImage = function(originalMessage, location) {
   var self = this;
-  imgur.uploadFile(location).then(function (json) {
-        console.log(json.data.link);
-        self._sendMessage(originalMessage, json.data.link)
-    })
-    .catch(function (err) {
-        console.error(err.message);
-        self._sendMessage(originalMessage, json.data.link, err)
-    });
-}
-
-Toby.prototype._sendMessage = function (originalMessage, url, err) {
-  var self = this;
-  var channel = self._getChannelById(originalMessage.channel);
-  if (!err) {
-    self.postMessageToChannel(channel.name, "Here\'s the table! " + url, { as_user: true});
-    console.log("Success!");
-  } else {
-    self.postMessageToChannel(channel.name, "Oh noes! Something went wrong.", { as_user: true});
-    console.log("Failure! Message Sent though!");
-  }
-
-}
-
-
-Toby.prototype._loadBotUser = function () {
-    var self = this;
-    this.user = this.users.filter(function (user) {
-        return user.name === self.name;
-    })[0];
+  var upload = new Slack(self.settings.token);
+  upload.uploadFile(
+    {
+      file: fs.createReadStream(path.join(__dirname, location)),
+      filetype: "jpg",
+      title: "Harvies - Coffee Machine",
+      initialComment: "This tha coffee table",
+      channels: self._getChannelById(originalMessage.channel).name
+    },
+    function(err, data) {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log("Uploaded file details: ", data);
+      }
+    }
+  );
 };
 
-Toby.prototype._isChatMessage = function (message) {
-    return message.type === 'message' && Boolean(message.text);
+Toby.prototype._loadBotUser = function() {
+  var self = this;
+  this.user = this.users.filter(function(user) {
+    console.log(user.name);
+    return user.name === self.name;
+  })[0];
 };
 
+Toby.prototype._isChatMessage = function(message) {
+  return message.type === "message" && Boolean(message.text);
+};
 
-Toby.prototype._isChannelConversation = function (message) {
-    return typeof message.channel === 'string' &&
-        message.channel[0] === 'C'
-        ;
+Toby.prototype._isChannelConversation = function(message) {
+  return typeof message.channel === "string" && message.channel[0] === "C";
 };
 
 /**
@@ -104,9 +96,11 @@ Toby.prototype._isChannelConversation = function (message) {
  * @returns {boolean}
  * @private
  */
-Toby.prototype._isMentioningToby = function (message) {
-    return message.text.toLowerCase().indexOf('table free') > -1 ||
-        message.text.toLowerCase().indexOf(this.name) > -1;
+Toby.prototype._isMentioningToby = function(message) {
+  return (
+    message.text.toLowerCase().indexOf("table free") > -1 ||
+    message.text.toLowerCase().indexOf(this.name) > -1
+  );
 };
 
 /**
@@ -115,8 +109,8 @@ Toby.prototype._isMentioningToby = function (message) {
  * @returns {boolean}
  * @private
  */
-Toby.prototype._isFromToby = function (message) {
-    return message.user === this.user.id;
+Toby.prototype._isFromToby = function(message) {
+  return message.user === this.user.id;
 };
 
 /**
@@ -125,10 +119,10 @@ Toby.prototype._isFromToby = function (message) {
  * @returns {Object}
  * @private
  */
-Toby.prototype._getChannelById = function (channelId) {
-    return this.channels.filter(function (item) {
-        return item.id === channelId;
-    })[0];
+Toby.prototype._getChannelById = function(channelId) {
+  return this.channels.filter(function(item) {
+    return item.id === channelId;
+  })[0];
 };
 
 module.exports = Toby;
--- webcam.js
+++ webcam.js
@@ -1,22 +1,21 @@
 var RaspiCam = require("raspicam");
 
-var camera = new RaspiCam({ mode: 'photo', output: './images/photo.jpg'});
+var camera = new RaspiCam({ mode: "photo", output: "./images/photo.jpg" });
 
-var capture = function (time) {
-  var p = new Promise( function( res, rej ) {
-      camera.start();
-
-      camera.on("read", function(err, timestamp, filename){
-        console.log('Stopping Camera');
-         setTimeout(function () {
-           camera.stop();
-         }, 500);
-      });
-      camera.on("stop", function() {
-        res("./images/photo.jpg");
-      });
+var capture = function(time) {
+  var p = new Promise(function(res, rej) {
+    camera.start();
+    camera.on("read", function(err, timestamp, filename) {
+      console.log("Stopping Camera");
+      setTimeout(function() {
+        camera.stop();
+      }, 200);
     });
+    camera.on("stop", function() {
+      res("./images/photo.jpg");
+    });
+  });
   return p;
-}
+};
 
 module.exports = capture;
